f396f6a22e8aec3efeec469f6b2000ed97b68b1b
nimethon <nazimulislam121@gmail.com>
2024-08-28 18:00:20 +0600

    first commit

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Inventory = () => {
+    const handleAddInventory = () =>{
+        
+    }
+    return (
+        <div>
+            <h1>Add Inventory to Sell more...</h1>
+            <h1> <li>srabon madari</li>
+            <li>maruf chutia</li>
+            <li>emon paglachud</li>
+            <li>kader maw-madari</li>
+            <li>sujon koromchod</li>
+            <li>rana dendi khor </li></h1>
+            <button onClick={handleAddInventory} >Add Inventor</button>
+        </div>
+    );
+};
+
+export default Inventory;
